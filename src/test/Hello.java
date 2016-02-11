9df4e553928f5c48d3bed8742a54d4261e25385e
SimonHGR <simon@dancingcloudservices.com>
2016-02-11 11:06:34 -0800

    Examples of String use, comparison, and interning

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -23,31 +23,45 @@ public class Hello {
     public static void main(String[] args) throws Throwable {
         System.out.println("Hello, world!");
         showThis(" Mrs. Jones ", 42);
-        
+
         boolean isWarm = true;
-        
+
         byte b = 42;
         short s = 3456;
         int i = 1234567;
         long l = 123456789012L;
         long l1;
-        
-//        l = l1 + 2;
 
+//        l = l1 + 2;
         float f = 3.14159F;
         double d = 3.14159;
-        
-        i = (int)l;
-        
+
+        i = (int) l;
+
         char c = '\ucafe';
-        
-        s = (short)(s + b);
+
+        s = (short) (s + b);
         s += b;
-        
+
         System.out.print("Enter text: ");
         Scanner sc = new Scanner(System.in);
         String line = sc.nextLine();
         System.out.println("You said: " + line);
+
+        String hello = "Hello";
+
+        hello.toUpperCase();
+        System.out.println(hello);
+
+        hello = hello.toUpperCase();
+        System.out.println(hello);
+        String h2 = "HELLO";
+
+        System.out.println("hello == h2 " + (hello == h2));
+        System.out.println("hello.equals(h2) " + (hello.equals(h2)));
+
+        hello = hello.intern();
+        System.out.println("hello == h2 " + (hello == h2));
     }
 
 }
